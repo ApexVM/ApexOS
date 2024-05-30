4bae09ac04ba0b8b551daf21ccfdc420663011f4
Slara <162091285+slara6804@users.noreply.github.com>
2024-05-30 22:20:09 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,10 @@
+# The direct flasher to install ApexOS - the hard way
 
+# Depences
+OS_BIN_DIR=/usr/local/
+
+# Main app
+echo "[WARN] This will flash ApexOS to your device."
+echo "Waiting 5 seconds before continuing..."
+sleep 5
+# I need to get all scripts and test them :3
