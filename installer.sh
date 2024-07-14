43fcc49d3f8deac4447293dca654205e62020e49
Slara <162091285+slara6804@users.noreply.github.com>
2024-07-14 23:42:43 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,22 @@
 # The direct flasher to install ApexOS - the hard way
 
 # Depences
-OS_BIN_DIR=/usr/local/
+OS_BIN_DIR=/usr/local/bin
+OS_ETC_DIR=/usr/local/etc
 
 # Main app
 echo "[WARN] This will flash ApexOS to your device."
 echo "Waiting 5 seconds before continuing..."
 sleep 5
 clear
+sudo mkdir /usr/local/etc/apexos/oldos/
 echo "Installing to $OS_BIN_DIR"
-wget 
+wget https://raw.githubusercontent.com/ApexVM/ApexOS/main/vpkg.cxx
+wget https://raw.githubusercontent.com/ApexVM/ApexOS/main/os.properties
+sudo mv /etc/os-properties /usr/local/etc/apexos/oldos/osprops
+g++ vpkg.cxx -o vpkg
+sudo mv vpkg /usr/local/bin/ && sudo chmod 777 /usr/local/bin/vpkg
+sudo mv os.properties /etc/os-properties
+clear
+echo "The base instalation is done"
 # I need to get all scripts and test them :3
