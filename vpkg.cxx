2c044acdd1f3edced7e5a791b9c7b6c7e4b97dcd
Slara <162091285+slara6804@users.noreply.github.com>
2024-07-16 17:26:37 +0200

    Update vpkg.cxx

--- vpkg.cxx
+++ vpkg.cxx
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        cerr << "Usage: " << argv[0] << " --install|--uninstall|--flash <package>" << endl;
+        cerr << "Usage: " << argv[0] << " --install|--uninstall|--flash <package> OR --update|--upgrade|--osupdate" << endl;
         return 1;
     }
 
@@ -35,6 +35,33 @@ int main(int argc, char *argv[]) {
             cout << "Flash successful!" << endl;
         } else {
             cerr << "Flash failed!" << endl;
+        }
+        else if (strcmp(argv[1], "--update") == 0) {
+        cout << "Updating packages listing" << endl;
+        string flashCommand = "apt update";
+        int result = system(flashCommand.c_str());
+        if (result == 0) {
+            cout << "Update successful!" << endl;
+        } else {
+            cerr << "Update failed!" << endl;
+        }
+        else if (strcmp(argv[1], "--upgrade") == 0) {
+        cout << "Upgrading packages" << endl;
+        string flashCommand = "apt upgrade -y";
+        int result = system(flashCommand.c_str());
+        if (result == 0) {
+            cout << "Upgrade successful!" << endl;
+        } else {
+            cerr << "Upgrade failed!" << endl;
+        }
+        else if (strcmp(argv[1], "--osupdate") == 0) {
+        cout << "Flashing: " << argv[2] << endl;
+        string flashCommand = "apt dist-upgrade";
+        int result = system(flashCommand.c_str());
+        if (result == 0) {
+            cout << "System update successful!" << endl;
+        } else {
+            cerr << "System update failed!" << endl;
         }
     } else {
         cerr << "Unknown option: " << argv[1] << endl;
